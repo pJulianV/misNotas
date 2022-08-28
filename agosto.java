9b4db6fc2f0498f3597210f7806a7cb6a15c5bfe
MyJulian <101911814+pJulianV@users.noreply.github.com>
2022-08-28 16:30:37 -0500

    notas :>

---
+++ agosto.java
@@ -0,0 +1,47 @@
+package aNotas;
+// * 12.08.VI
+
+    // ? Como Elaborar un diagrama de clases
+    // ? Paso 1: Extraer los actores que serian las clases
+    // ? Paso 2: 
+    // ? Paso 3: Agregacion Acumulacion 
+    // ? Paso 4:
+    // ? Paso 5: Asociacion, Herencia - Generalizacion, Dependencia
+
+// * 13.08.SA
+
+    // En las clases abstractas se crean metodos sobreescribibles
+    // @Override = Forzar al compilador a revisar que se este sobrescribiendo correctamente el
+    // metodo
+
+    // Las clase abstractas y las interfases tienen en comun el sobrescribir
+
+    // ? Cual es la diferencia principal entre clase abstracta e interfas
+
+    // Se puede generar codigo (OOP) con easyUML, pog!
+
+// * 17.08.MI
+    // Controles visuales:
+    // Eventos: Son los sucesos que hacen que cierta accion conlleve una ejecucion
+    // Eventos visuales
+    // Propiedades: Caracteristicas (titulo, nombre, tamaño, color)
+    // Eventos: Acciones
+
+    // Aprender a usar Interfas Grafica de Usuario(GUI) Swing J-Frame (Programas sin terminales :>)
+// * 21.08.DO
+    // ? Una interfas se usa para "reutilizar" metodos
+    // ? Mientras que una clase abstracta se usa para "reutilizar" atributos
+
+// * 25.08.JU
+    // ? MySQL (Quiero usarlo para "jerarquia empresa")
+    // Conceptos:
+    // ? Muchos a muchos, uno a muchos, uno a uno
+    // ? Llaves foraneas
+
+// * 27.08.SA
+    // ? Mesero => Cocina => Ingredientes
+    // ? Bibliotecario 
+    //      ^         ^            ^
+    // API     =>  Frontend  => Backend(Servidor)
+    // ? API is like a Bibliotecario
+    // Coding Dojo => Organizar style
