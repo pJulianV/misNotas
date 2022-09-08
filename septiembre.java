48231af94c9f8894259190b767a6f580d7b7b8fb
MyJulian <101911814+pJulianV@users.noreply.github.com>
2022-09-07 20:47:30 -0500

    :>

--- septiembre.java
+++ septiembre.java
@@ -1,5 +1,6 @@
 // * 07.09.MI
 // Introduccion al Desarrollo de software
 // Terminos:
-// SCRUM, Jira, Git (GitLab, GitHub) 
+// SCRUM, Jira, Git (GitLab, GitHub) , Frontend, Linux POG
+// Es interesante lo de trabajar en equipo
 // Se asigno una carpeta con ideas para proyecto y Sprint 1
