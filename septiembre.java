19f110b9610a4c65c46fe5bfe71a455a5f41d229
MyJulian <101911814+pJulianV@users.noreply.github.com>
2022-09-07 22:07:07 -0500

    final 07.09.MI

--- septiembre.java
+++ septiembre.java
@@ -1,10 +1,24 @@
 // * 07.09.MI
 // Introduccion al Desarrollo de software
 // Terminos:
-// SCRUM, Jira, Git (GitLab, GitHub) , Frontend, Linux POG
+// SCRUM, agile, Jira, Git (GitLab, GitHub) , Frontend, Linux POG
 // Es interesante lo de trabajar en equipo
 // Se asigno una carpeta con ideas para proyecto( Papeleria, Administracion y Hospital) y Sprint 1
 
 // Para poder desarrollar los contenidos y Sprints del ciclo, el beneficiario trabajará con el editor de texto Visual Studio Code, con lenguajes Java, HTML, CSS y JavaScript. Además. También se debe utilizar algunos frameworks, como Spring Boot y Bootstrap, Jira, GitHub - GitLab y MySQL. 
 // En la plataforma LMS, el beneficiario podrá encontrar los videos y tutoriales que le ayudarán a resolver sus dudas .
 // Cada equipo, contará con el acceso a un servidor remoto en el cual podrá administrar, configurar e implantar en modo producción su proyecto WEB
+
+// Analisis:
+// Lo que se pide => Requerimientos => socialicacion y aprobacion (Usuario)
+
+// Diseño:
+// Modelado Arquetectura de datos => Modelamientos de procesos => Seguridad y respaldo => Interfaz (Front-end)
+
+// Desarrollo:
+// Esquema de las bases de datos => Programacion
+
+// ! Programar != que desarrollar, pero para desarrollar tienes que programar
+
+// Implementation
+// Pruebas (de componentes, unitarias, de humo) => Ajustes => Puesta en Funcionamiento
